6f0c7aefe5a8df91bbf98a47a483c588dcd8428a
Christian Diether Reyes <cbreyes4@up.edu.ph>
2019-10-19 15:17:06 +0800

    moved or renaed files

--- resources/views/SYSMG/functional_units.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="col-md-8  main">
-    <h1 class="page-header">Functional Units</h1>
-</div>
--- resources/views/SYSMG/signatories.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="col-md-8  main">
-    <h1 class="page-header">Signatories</h1>
-</div>
