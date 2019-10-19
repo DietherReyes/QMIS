dce28f89920bfaffcfffb2d7590234e6a1c81b3b
Christian Diether Reyes <cbreyes4@up.edu.ph>
2019-10-19 15:22:37 +0800

    modified sidebar links

---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="col-md-3 sidebar">
+  <ul class="nav nav-sidebar">
+    <li><a href="/sysmg/accounts">User Account Management</a></li>
+    <li><a href="/sysmg/units">Functional Units</a></li>
+    <li><a href="/sysmg/signatories">Signatories</a></li>
+  </ul>
+</div>
+   
