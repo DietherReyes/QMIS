958b787a0b427b7b14c3e457faa8058612d23b54
Christian Diether Reyes <cbreyes4@up.edu.ph>
2019-11-15 21:20:17 +0800

    model for management review module

---
+++ app/ManagementReview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ManagementReview extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'meeting_name', 'venue', 'date', 'minutes', 'action_plan', 'agenda_memo', 'attendance_sheet', 'presentation', 'description'
+    ];
+}
