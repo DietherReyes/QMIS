98d482448858ca59b50635f281ec210324d890f6
Christian Diether Reyes <cbreyes4@up.edu.ph>
2019-11-18 03:29:51 +0800

    model for csm module

---
+++ app/CustomerOverallRating.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerOverallRating extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'response_delivery', 'work_quality', 'personnels_quality', 'overall_rating', 'csm_id'
+    ];
+
+    public function customer_overall_rating()
+    {
+        return $this->belongsTo('App\CustomerSatisfactionMeasurement');
+    }
+}
---
+++ app/CustomerSatisfactionMeasurement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerSatisfactionMeasurement extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'functional_unit', 'year', 'quarter', 'total_customer', 'total_male', 'total_female', 'comments'
+    ];
+}
