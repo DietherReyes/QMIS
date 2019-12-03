c131df5cd01907fffa6adffba68f0c48ae599a41
Christian Diether Reyes <cbreyes4@up.edu.ph>
2019-12-03 15:10:10 +0800

    updated sidebar ui

--- resources/views/include/account_sidebar.blade.php
+++ resources/views/include/account_sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-2 sidebar">
-  <ul class="nav nav-sidebar">
+  <ul class="nav nav-pills nav-stacked">
     <li class="active"><a href="/sysmg/accounts">User Account Management</a></li>
     <li><a href="/sysmg/units">Functional Units</a></li>
     <li><a href="/sysmg/signatories">Signatories</a></li>
--- resources/views/include/add_section_sidebar.blade.php
+++ resources/views/include/add_section_sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="col-sm-3 col-md-1 sidebar">
-    <ul class="nav nav-sidebar">
+        <ul class="nav nav-pills nav-stacked">
         <li><a href="/qmsd/create">Add QMSD</a></li>
         <li  class="active"><a href="/qmsd/sections/idx">Add Section</a></li>
     </ul>
