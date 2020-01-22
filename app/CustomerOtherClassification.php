9a8ca49bcc114a834d3059f880aedb46054fc38d
Christian Diether Reyes <cbreyes4@up.edu.ph>
2020-01-22 18:53:23 +0800

    Added new models

---
+++ app/CustomerOtherClassification.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerOtherClassification extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'count', 'csm_id'
+    ];
+
+    public function customer_other_classification()
+    {
+        return $this->belongsTo('App\CustomerSatisfactionMeasurement');
+    }
+}
