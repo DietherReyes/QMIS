bb0365258401699c6a6f076d241d3114a4c97ce9
Christian Diether Reyes <cbreyes4@up.edu.ph>
2020-01-22 18:56:11 +0800

    added new sidebars for csm

---
+++ resources/views/include/add_csm_sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="col-sm-3 col-md-2 sidebar">
+    <ul class="nav nav-pills nav-stacked">
+    <li class="active"><a href="/csm/create">Add CSM</a></li>
+    <li><a href="/csm/addresses/idx">Addresses</a></li>
+    <li><a href="/csm/services/idx">Services</a></li>
+    </ul>
+</div>
+    
