679cd3884c35652ae4f65f79ca2e71445829584b
Christian Diether Reyes <cbreyes4@up.edu.ph>
2020-02-14 16:13:45 +0800

    updated ui

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
 
-        <h1>Welcom to Quality Management Information System</h1>
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+            @guest
+                <div class="jumbotron text-center">
+                        
+                    <h1>Welcome to Quality Management Information System</h1>
+            
+                    
+                    <p> <a class="btn btn-primary btn-lg" href="{{route('login')}}">LOGIN</a> </p>
+                        
+                </div>
+            @else 
+            <div class="row placeholders">
+                    <div class="jumbotron text-center">
 
-        @guest
-            <p> <a class="btn btn-primary btn-lg" href="{{route('login')}}">LOGIN</a> </p>
-        @else
-            {{-- <ul>
-                <li> <a class="btn btn-primary btn-lg" style="width:300px" href="/csm">CSM</a> </li>
-                @if(Auth::user()->role === 'admin')
-                <li>  <a class="btn btn-primary btn-lg" style="width:300px" href="/sysmg/accounts">SYSTEM MANAGEMENT</a> </li>
-                @endif
-            </ul>    
-         --}}
-        @endguest
+                            <h1>Welcome to Quality Management Information System</h1>
+                
+                                
+                        </div>
+
+                    <div class="col-md-4 ">
+                        <img class="center" src="/storage/assets/csm.png">
+                        
+                        <div class="placeholder">
+                                <h4> <strong> Customer Satisfaction Measurement </strong> </h4>
+                        </div>
+                        <br>
+
+                        <p>This module consist of list of Customer Satisfaction Measurements for each functional unit of the DOST-CALABARZON added by an authorized users which can be viewed and edited by an authorized user.</p>
+
+                        <div class="placeholder">
+                                <a class="btn btn-primary btn-md center"  href="/csm">GO</a> 
+                        </div>
+                        
+                       
+                        
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <img class="center" src="/storage/assets/mr.png">
+                        <div class="placeholder">
+                                <h4> <strong> Management Review </strong> </h4>
+                        </div>
+                        <br>
+
+                        <p>This module consist of list of meetings held within DOST-CLABARZON office added by an authorized users which can be viewed or edited by authorized users.</p>
+
+                        <div class="placeholder">
+                                <a class="btn btn-primary btn-md center"  href="/manrev">GO</a> 
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-4 ">
+                        <img class="center" src="/storage/assets/qmsd.png">
+                        <div class="placeholder">
+                                <h4> <strong> Quality Management System Documentation </strong> </h4>
+                        </div>
+
+                        <p>This module consist of list of pages of the Quality Managament System Manual of DOST  added by an authorized users which can be viewed or edited by authorized users..</p>
+
+                        <div class="placeholder">
+                                <a class="btn btn-primary btn-md center"  href="/qmsd">GO</a> 
+                        </div>
+                    </div>
+
+                    
+                    
+            </div>   
+            @endguest
+
     </div>
+    <div class="col-md-1"></div>
+</div>
+    
 @endsection
--- resources/views/pages/unauthorized.blade.php
+++ resources/views/pages/unauthorized.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-
-        <h1>Unauthorized access</h1>
-
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 jumbotron text-center">
+            <h1>Unauthorized access</h1>
+            <a href="{{url()->previous()}}" class="btn  btn-md btn-primary">BACK</a>
+        </div>
+        <div class="col-md-3"></div>
     </div>
+    
 @endsection
