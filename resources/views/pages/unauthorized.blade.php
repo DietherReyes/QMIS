4aa70dfed1d0103437ccf84bbde7da503bdbf5b5
Christian Diether Reyes <cbreyes4@up.edu.ph>
2020-07-30 16:58:52 +0800

    added new page for deactivated account

--- resources/views/pages/unauthorized.blade.php
+++ resources/views/pages/unauthorized.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6 jumbotron text-center">
-            <h1>Unauthorized access</h1>
+            <h1>Unauthorized Access</h1>
             <a href="{{url()->previous()}}" class="btn  btn-md btn-primary">BACK</a>
         </div>
         <div class="col-md-3"></div>
