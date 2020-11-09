e3166954b675337d59d38c9f96e579fdb0f7ce8c
Christian Diether Reyes <cbreyes4@up.edu.ph>
2020-11-09 08:22:37 +0800

    added logs sidebar

--- resources/views/include/signatories_sidebar.blade.php
+++ resources/views/include/signatories_sidebar.blade.php
@@ -3,6 +3,7 @@
       <li ><a href="/sysmg/accounts">User Account Management</a></li>
       <li ><a href="/sysmg/units">Functional Units</a></li>
       <li class="active"><a href="/sysmg/signatories">Signatories</a></li>
+      <li><a href="/sysmg/logs">Logs</a></li>
     </ul>
   </div>
      
